e7989bf85e8e0336e56315a399b60d1e5a02c688
GuFan <1491239841@qq.com>
2018-07-10 19:29:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/happyanswer/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.administrator.happyanswer;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.administrator.happyanswer.adapter.MainViewPagerAdapter;
+import com.example.administrator.happyanswer.fragment.HomeFragment;
+import com.example.administrator.happyanswer.fragment.UserFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView navigation;
+    private ViewPager viewPager;
+    private List<Fragment>frameLayouts;
+    private MainViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFragment();
+        initView();
+
+    }
+    private void initView(){
+        navigation = (BottomNavigationView) findViewById(R.id.main_navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        viewPager.setCurrentItem(0);
+                        return true;
+                    case R.id.navigation_dashboard:
+                        viewPager.setCurrentItem(1);
+                        return true;
+                }
+                return false;
+            }
+        });
+        viewPager = (ViewPager) findViewById(R.id.main_viewPager);
+        adapter = new MainViewPagerAdapter(getSupportFragmentManager(),frameLayouts);
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                navigation.setSelectedItemId(navigation.getMenu().getItem(position).getItemId());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+    private void initFragment(){
+        frameLayouts = new ArrayList<>();
+        frameLayouts.add(new HomeFragment());
+        frameLayouts.add(new UserFragment());
+    }
+
+}
